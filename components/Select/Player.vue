b8c9d772d10987ad6898079a56f74e98c23c6d59
erikleisinger@gmail.com <erikleisinger>
2023-07-15 17:15:00 -0400

    auto select player based on team & shot no

--- components/Select/Player.vue
+++ components/Select/Player.vue
@@ -26,7 +26,7 @@ import {useDataStore} from "@/store/data";
 import {useGameStore } from '@/store/game'
 const props = defineProps({
     player: String,
-    onlyThrowing: Boolean,
+    filter: Boolean,
 })
 const emit = defineEmits(['update:modelValue'])
 const editedPlayer = computed({
@@ -46,27 +46,11 @@ const getPlayers = async (force) => {
   loadingPlayers.value = false;
 };
 
-/**
- * FILTER PLAYERS BY THOSE ON THE CURRENTLY THROWING TEAM
- * IF: props.onlyThrowing = true;
- */
-
-const gameStore = useGameStore();
-
-const throwingTeam = computed(() => gameStore.getThrowingTeamId(gameStore.shot))
-
-const isPlayerOnCurrentTeam = (player, currentThrowingTeam) => {
-  const {id} = player;
-  const {lead_player_id, second_player_id, third_player_id, fourth_player_id, fifth_player_id, sixth_player_id, seventh_player_id} = currentThrowingTeam;
-  return [lead_player_id, second_player_id, third_player_id, fourth_player_id, fifth_player_id, sixth_player_id, seventh_player_id].some((p) => p?.id === id)
-
-}
 
 const playerOptions = computed(() => {
   let players = [...store.players];
-  if (props.onlyThrowing) {
-      const currentThrowingTeam = store.teams.find((t) => t.id === throwingTeam.value)
-    players = players.filter((p) => isPlayerOnCurrentTeam(p, currentThrowingTeam))
+  if (props.filter) {
+    players.filter(props.filter)
   }
   const {formatPlayerForSelection} = useFormat();
   return players.map((d) => formatPlayerForSelection(d));
