3daf55a920b9cc18f6b68f4a3ae3f825d4debe68
erikleisinger@gmail.com <erikleisinger>
2023-07-26 16:59:45 -0400

    slight update to team stats page

---
+++ components/Stats/Card.vue
@@ -0,0 +1,27 @@
+<template>
+    <div class="stats__card pretty-shadow column justify-center items-center simple-border" :style="{height: size, width: size}">
+        <div class="content">
+            <h1>{{props.value}}</h1>
+        </div>
+        <div class="label">
+            {{props.label}}
+        </div>
+    </div>
+</template>
+<style lang="scss" scoped>
+    .stats__card {
+        padding: 16px;
+        border-radius: 16px;
+        .content {
+
+        }
+
+    }
+</style>
+<script setup>
+    const props = defineProps({
+        label: String,
+        size: String,
+        value: [String, Number, null]
+    })
+</script>
--- pages/stats/team/[id].vue
+++ pages/stats/team/[id].vue
@@ -1,21 +1,50 @@
 <template>
-    <label>Game control</label>
-    <div class="subtitle">% hammer possession of all ends played</div>
-    <div class="percent">{{controlOfGame}}</div>
-    <label>Steal efficiency</label>
-    <div class="subtitle">without hammer, % ends scored</div>
-    <div class="percent">{{stolenEnds}}</div>
-     <label>Hammer efficiency</label>
-    <div class="subtitle">with hammer, % time scored 2+ points</div>
-    <div class="percent">{{hammerEfficiency}}</div>
+
+<div class="row justify-around">
+    <StatsCard label="Wins" :value="wins" size="30%"/>
+    <StatsCard label="Draws" :value="ties" size="30%"/>
+    <StatsCard label="Losses" :value="losses" size="30%"/>
+</div>
+<div class="game-breakdown">
+    <div>
+        <h1>Offense</h1>
+          <h2 >With hammer</h2>
+   <label>Hammer possession</label>
+    <div class="percent">{{controlOfGame}}</div> 
+
+     <label>Hammer conversion</label>
+    <div class="subtitle">2+ points scored</div>
+    <div class="percent">{{hammerConversion}}</div>
       <label>Blank ends</label>
-    <div class="subtitle">with hammer, % of ends blanked</div>
     <div class="percent">{{blankEnds}}</div>
         <label>Forced with hammer</label>
-    <div class="subtitle">with hammer, % of ends forced</div>
     <div class="percent">{{forcedWith}}</div>
+    </div>
+    <div class="text-right">
+         <h1 >Defense</h1>
+         <h2 >Without hammer</h2>
+             <label>Steal efficiency</label>
+    <div class="percent">{{stolenEnds}}</div>
+          <label>Force efficiency</label>
+    <div class="percent">{{forceWithout}}</div>
+              <label>Average points conceded</label>
+    <div>{{avgPointsConceded}}</div>
+    </div>
+
+    </div>
 </template>
 <style lang="scss" scoped>
+.game-breakdown {
+    display: grid;
+    grid-template-columns: repeat(2, 1fr);
+    padding: 8px;
+    h2 {
+        font-size: 1.2em;
+        margin-top: -8px;
+        font-style: italic;
+        margin-bottom: 16px;
+    }
+}
 label {
     font-weight: bolder;
 }
@@ -33,24 +62,31 @@ label {
 <script setup>
     const route = useRoute();
     const stats = ref(null)
+    const wins = ref(0);
+    const losses = ref(0);
+    const ties = ref(0)
     const controlOfGame = ref(null)
     const stolenEnds = ref(null)
-    const hammerEfficiency = ref(null)
+    const hammerConversion = ref(null)
     const blankEnds = ref(null)
     const forcedWith = ref(null)
-    onMounted(async() => {
-        const client = useSupabaseClient();
+    const forceWithout = ref(null)
+    const avgPointsConceded = ref(null);
+    const getGameStats = async () => {
+         const client = useSupabaseClient();
         const {data} = await client.rpc('get_team_stats', {
             team_id_param: route.params.id
         })
         const [s] =data;
         stats.value = s
         const {
+            avg_points_conceded,
+            forced_end_count,
             hammer_1_point_count,
-            total_ends_played,
             hammer_blank_count,
             hammer_ends_count,
-            stolen_end_count
+            stolen_end_count,
+            total_ends_played
         } = stats.value;
         
         // Of all ends played, what % of the time did the team have the hammer?
@@ -60,7 +96,7 @@ label {
         stolenEnds.value = toPercent(stolen_end_count / (total_ends_played - hammer_ends_count))
 
         // Hammer efficiency -- scored 2+ points with hammer. NOT INCLUDING blanks
-        hammerEfficiency.value = toPercent(addPointCounts(2, 8) / (hammer_ends_count - hammer_blank_count))
+        hammerConversion.value = toPercent(addPointCounts(2, 8) / (hammer_ends_count - hammer_blank_count))
 
         // Blank ends: % of ends with hammer when score was 0
         blankEnds.value = toPercent(hammer_blank_count / hammer_ends_count)
@@ -68,6 +104,26 @@ label {
         // forced with hammer: % of ends with hammer when team only scored 1
         forcedWith.value = toPercent(hammer_1_point_count / (hammer_ends_count - hammer_blank_count))
 
+        // forced opposition to take one when they had hammer
+        forceWithout.value = toPercent(forced_end_count / (total_ends_played - hammer_ends_count))
+
+        // Average points conceded without hammer
+        avgPointsConceded.value = avg_points_conceded.toFixed(2);
+    }
+
+    const getWinsLossess = async () => {
+         const client = useSupabaseClient();
+        const {data} = await client.rpc('get_team_wins', {
+            team_id_param: route.params.id
+        })
+        wins.value = data.wins;
+        losses.value = data.losses
+        ties.value = data.ties;
+    }
+    onMounted(async() => {
+    getGameStats();
+
+    getWinsLossess();
     })
 
 
