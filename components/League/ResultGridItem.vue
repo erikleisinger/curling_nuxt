d56153be34fea688a9c1b4e08ca13045238013e3
erikleisinger@gmail.com <erikleisinger>
2023-12-29 12:25:04 -0500

    league results grid view

---
+++ components/League/ResultGridItem.vue
@@ -0,0 +1,34 @@
+<template>
+<div v-if="!result" class="blank"/>
+    <div v-else-if="teamOnePoints === teamTwoPoints">-</div>
+
+    <div class="row no-wrap justify-center items-center full-height" v-else>
+        <div style="width: 25px" v-if="teamOnePoints !== teamTwoPoints">
+            <TeamAvatar
+                :teamId="
+                    teamOnePoints > teamTwoPoints
+                        ? Number(Object.keys(props.result)[0])
+                        : Number(Object.keys(props.result)[1])
+                "
+            />
+        </div>
+        <div class="text-xs q-ml-xs">
+            +{{ Math.abs(teamOnePoints - teamTwoPoints) }}
+        </div>
+    </div>
+</template>
+<style lang="scss" scoped>
+    .blank {
+        background-color: rgba(0,0,0,0.2);
+        height: 100%;
+        width: 100%;
+    }
+</style>
+<script setup>
+const props = defineProps({
+    result: Object,
+});
+
+const teamOnePoints = computed(() => Object.values(props.result)[0]);
+const teamTwoPoints = computed(() => Object.values(props.result)[1]);
+</script>
--- components/League/Results.vue
+++ components/League/Results.vue
@@ -1,5 +1,42 @@
 <template>
-    <div v-if="!isLoading && games.length">
+    <div class="results__scroller">
+        <div class="results__grid">
+            <div class="results__teams-x grid-row">
+                <div />
+                <div
+                    class="row justify-center full-width full-height items-center grid-column"
+                    v-for="team in league?.teams"
+                    :key="team.team_id"
+                >
+                    <div style="width: 30px">
+                        <TeamAvatar :teamId="team.team_id" />
+                    </div>
+                </div>
+            </div>
+            <div
+                v-for="(team, xIndex) in league?.teams"
+                :key="team.team_id"
+                class="grid-row"
+            >
+                <div
+                    class="full-height full-width row justify-center items-center grid-column"
+                >
+                    <div style="width: 30px">
+                        <TeamAvatar :teamId="team.team_id" />
+                    </div>
+                </div>
+                <div
+                    v-for="(team, yIndex) in league?.teams"
+                    :key="team.team_id"
+                    class="text-bold text-lg text-center grid-column"
+                >
+                    <LeagueResultGridItem :result="getResult(xIndex, yIndex)" />
+                </div>
+            </div>
+        </div>
+    </div>
+     <div v-if="!isLoading && games.length" class="q-mt-lg">
+     
         <div
                 v-for="game in games"
                 :key="game.id"
@@ -12,13 +49,37 @@
             </div>
     </div>
 </template>
+<style lang="scss" scoped>
+.results__scroller {
+    overflow: auto;
+}
+.results__grid {
+    display: grid;
+    grid-template-rows: v-bind(rows);
+    min-width: v-bind(minWidth);
+
+    .grid-row {
+        display: grid;
+        grid-template-columns: v-bind(columns);
+    }
+    .grid-column {
+        border-right: 1px solid rgba(0, 0, 0, 0.2);
+        border-bottom: 1px solid rgba(0, 0, 0, 0.2);
+    }
+}
+</style>
 <script setup>
 import { useQuery } from "@tanstack/vue-query";
 import Game from "@/store/models/game";
 import Rink from "@/store/models/rink";
+import League from "@/store/models/league";
 const props = defineProps({
     leagueId: Number,
 });
+
+const league = computed(() =>
+    useRepo(League).with("teams").where("id", props.leagueId).first()
+);
 
 const { getLeagueGames } = useGame();
 
@@ -42,6 +103,54 @@ const games = computed(() => {
             rink: useRepo(Rink).where("id", game.rink_id).first(),
         }));
 });
+
+const rows = computed(
+    () => `repeat(${(league.value?.teams?.length ?? 0) + 1}, 1fr)`
+);
+const columns = computed(
+    () => `repeat(${(league.value?.teams?.length ?? 0) + 1}, 1fr)`
+);
+
+const getResult = (x, y) => {
+    if (x === y) return null;
+    const { teams } = league.value;
+    const xTeam = teams[x];
+    const yTeam = teams[y];
+    // console.log(xTeam, xTeam.team_id)
+    const allGames = games.value?.filter(
+        ({ teams }) =>
+            teams.some(({ team_id }) => xTeam.team_id === team_id) &&
+            teams.some(({ team_id }) => yTeam.team_id === team_id)
+    );
+    if (!allGames?.length)
+        return {
+            [xTeam.team_id]: 0,
+            [yTeam.team_id]: 0,
+        };
+    const results = {
+        [xTeam.team_id]: 0,
+        [yTeam.team_id]: 0,
+    };
+    allGames.forEach((game) => {
+        const { points_scored: xTeamPoints } = game.teams.find(
+            ({ team_id }) => team_id === xTeam.team_id
+        );
+        const { points_scored: yTeamPoints } = game.teams.find(
+            ({ team_id }) => team_id === yTeam.team_id
+        );
+        if (xTeamPoints > yTeamPoints) {
+            results[xTeam.team_id] += 1;
+        } else if (yTeamPoints > xTeamPoints) {
+            results[yTeam.team_id] += 1;
+        }
+    });
+
+    return results;
+};
+
+const minWidth = computed(
+    () => `${((league.value?.teams?.length ?? 0) + 1) * 60}px`
+);
 </script>
 <script>
 export default {
